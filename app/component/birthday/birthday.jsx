6e4b30d2746549ff36f8da8a9aa673c6cc1a74c3
vipslord <my.yershov@gmail.com>
2017-06-19 13:35:55 +0300

    update birthday message

--- app/component/birthday/birthday.jsx
+++ app/component/birthday/birthday.jsx
@@ -1,41 +1,43 @@
 import React from 'react'
 import './birthday.styl'
 
+const ALL_YEARS_IN_MILISEC = 31557600000
+const ALL_DAYS_IN_MILISEC = 86400000
+
 class Birthday extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor () {
+    super()
     this.state = {
       birthdate: window._config.data.birthdate,
       birthdayStatus: ''
     }
   }
-  componentDidMount () {
-    const date = '2017-06-18'
-    // const age = ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000))
-    // console.log(age * 365.2564)
-    // const birthdate = new Date(this.state.birthdate).getTime()
-    const birthdate = new Date(date).getTime()
-    const curentDate = new Date().getTime()
-    const dayLeft = Math.ceil((birthdate.valueOf() - curentDate.valueOf()) / 86400000)
-    //  + Math.ceil(Math.round(age) * 365)
-    // console.log(Math.ceil((birthdate.valueOf() - curentDate.valueOf()) / 86400000))
-    // console.log(Math.ceil(Math.round(age) * 365.2564))
-    // console.log(dayLeft)
-    if (dayLeft < 8 && dayLeft > 1) {
-      this.setState({birthdayStatus: 'In ' + dayLeft + ' days'})
-    } else if (dayLeft > -8 && dayLeft < -1) {
-      this.setState({birthdayStatus: dayLeft * -1 + ' days ago'})
-    } else if (dayLeft === -1) {
-      this.setState({birthdayStatus: 'Yesterday'})
-    } else if (dayLeft === 1) {
-      this.setState({birthdayStatus: 'Tomorrow'})
-    } else if (dayLeft === 0) {
-      this.setState({birthdayStatus: 'Today'})
+  getBirthdaysStatus (birthdate) {
+    const age = Math.round((new Date().getTime() - new Date(birthdate)) / ALL_YEARS_IN_MILISEC) * ALL_YEARS_IN_MILISEC
+    const birthday = new Date(birthdate).getTime() + age
+    const currentDate = new Date().getTime()
+    const dayLeft = Math.ceil((birthday.valueOf() - currentDate.valueOf()) / ALL_DAYS_IN_MILISEC)
+    const day = {
+      '-1': 'Yesterday',
+      '1': 'Tomorrow',
+      '0': 'Today'
     }
+    let birthdayStatus
+    if (day[dayLeft]) {
+      birthdayStatus = day[dayLeft]
+    } else if (dayLeft < 8 && dayLeft > 1) {
+      birthdayStatus = 'In ' + dayLeft + ' days'
+    } else if (dayLeft > -8 && dayLeft < -1) {
+      birthdayStatus = dayLeft * -1 + ' days ago'
+    }
+    return birthdayStatus
+  }
+  componentDidMount () {
+    this.setState({birthdayStatus: this.getBirthdaysStatus(this.state.birthdate)})
   }
   render () {
     return (
-      <div id={this.state.birthdayStatus === '' ? 'birthday-wrap-disabled' : 'birthday-wrap'}><h1>{this.state.birthdayStatus}</h1></div>
+      <div id={this.state.birthdayStatus === undefined ? 'birthday-wrap-disabled' : 'birthday-wrap'}><h1>{this.state.birthdayStatus}</h1></div>
     )
   }
 }
