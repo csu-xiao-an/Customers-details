410b1e6022f0c6280651479e6544652e3221ce91
vipslord <my.yershov@gmail.com>
2017-07-14 17:05:11 +0300

    remove bootstrap, minimize swiper.css

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ import { Router, Route, hashHistory } from 'react-router'
 import Home from './components/home/home.jsx'
 import ReactDOM from 'react-dom'
 import React from 'react'
+import './main.styl'
 
 const client = window._config
 
