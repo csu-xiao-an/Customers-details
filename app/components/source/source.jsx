eacfc9732df2163ab285e8bd4b8d1aab7d142692
vipslord <my.yershov@gmail.com>
2017-08-01 16:30:10 +0300

    update source, add select-box and request,

--- app/components/source/source.jsx
+++ app/components/source/source.jsx
@@ -1,4 +1,7 @@
+import { updateService } from 'project-services'
+import 'react-select/dist/react-select.css'
 import React, { Component } from 'react'
+import Select from 'react-select'
 import './source.styl'
 
 const client = window._config
@@ -7,14 +10,35 @@ class Source extends Component {
   constructor () {
     super()
     this.state = {
-      isOpenSource: false
+      isOpenSource: false,
+      selectedValue: client.data.source
     }
     this.handleSource = this.handleSource.bind(this)
+    this.logChange = this.logChange.bind(this)
+    this.submit = this.submit.bind(this)
   }
   handleSource () {
     this.setState({ isOpenSource: !this.state.isOpenSource })
   }
+  logChange (val) {
+    val ? this.setState({selectedValue: val.value}) : this.setState({selectedValue: ''})
+  }
+  async submit () {
+    const url = client.urls.main + client.data.id
+    const method = 'PATCH'
+    const body = `source=${this.state.selectedValue}`
+    await updateService(url, method, body)
+  }
   render () {
+    const list = client.translations.source_list
+    const setValues = prop => ({value: list[prop], label: list[prop]})
+    let options = [
+      setValues('ads'),
+      setValues('fb_page'),
+      setValues('family'),
+      setValues('friends'),
+      setValues('recommendation')
+    ]
     return (
       <div id='source'>
         <div className={this.state.isOpenSource ? 'hidden' : client.data.source ? 'hidden' : 'add-source-wrap'}>
@@ -24,20 +48,14 @@ class Source extends Component {
         <div className={this.state.isOpenSource ? 'add-select-wrap' : client.data.source ? 'add-select-wrap' : 'hidden'}>
           <h1>{client.translations.source_arrival}</h1>
           <div className='button-wrap'>
-            <button>{client.translations.save}</button>
-          </div>
-          <div className='delete-wrap'>
-            <img className='delete' src={client.urls.media + 'add.svg'} onClick={() => { }} />
+            <button onClick={this.submit}>{client.translations.save}</button>
           </div>
           <div className='select-wrap'>
-            <select className='select'>
-              <option disabled className='option'>{client.data.source}</option>
-              <option value={client.translations.source_list.ads}><span>{client.translations.source_list.ads}</span></option>
-              <option value={client.translations.source_list.fb_page}>{client.translations.source_list.fb_page}</option>
-              <option value={client.translations.source_list.family}>{client.translations.source_list.family}</option>
-              <option value={client.translations.source_list.friends}>{client.translations.source_list.friends}</option>
-              <option value={client.translations.source_list.recommendation}>{client.translations.source_list.recommendation}</option>
-            </select>
+            <Select
+              value={this.state.selectedValue}
+              options={options}
+              onChange={this.logChange}
+            />
           </div>
         </div>
       </div>
--- services/request-service.js
+++ services/request-service.js
@@ -2,7 +2,7 @@ const mainRequestService = async (url, options) => {
   let res
   await fetch(url, options).then(response => {
     if (response.status === 503) {
-      setTimeout(mainRequestService(url, options), 1000)
+      setTimeout(() => { mainRequestService(url, options) }, 1000)
     }
     if (response.status === 400) {
       throw response
