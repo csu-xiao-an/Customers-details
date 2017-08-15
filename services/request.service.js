587f5f5bd694aa6e4e9551a7531792583cb79752
vipslord <my.yershov@gmail.com>
2017-08-15 12:27:09 +0300

    update request service (retry after)

--- services/request.service.js
+++ services/request.service.js
@@ -1,10 +1,8 @@
 const mainRequestService = async (url, options) => {
-  // url = 'http://api.bewebmaster.co.il/503'
   let res
   await fetch(url, options).then(response => {
-    console.log(response)
     if (response.status === 503) {
-      setTimeout(() => { mainRequestService(url, options) }, 1000)
+      setTimeout(() => { mainRequestService(url, options) }, response.headers.get('Retry-After') * 1000)
     }
     if (response.status === 400) {
       throw response
