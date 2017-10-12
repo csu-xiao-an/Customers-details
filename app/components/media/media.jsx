7c48a0ecfe46ee4f088839889422635c3739a623
vipslord <my.yershov@gmail.com>
2017-10-12 15:46:56 +0300

    fix img orientation

--- app/components/media/media.jsx
+++ app/components/media/media.jsx
@@ -1,6 +1,6 @@
+import {dataURLtoFile, getOrientation} from 'project-components'
 import MediaModal from '../media-modal/media-modal.jsx'
 import {mediaPostService} from 'project-services'
-import {dataURLtoFile} from 'project-components'
 import React, {Component} from 'react'
 import Swiper from 'react-id-swiper'
 import moment from 'moment'
@@ -60,41 +60,51 @@ class Media extends Component {
     }
   }
   resize = f => {
-    let dataURL
-    let img = document.createElement('img')
-    let reader = new FileReader()
-    reader.readAsDataURL(f)
-    reader.onload = () => {
-      this.setState({ imagePreviewUrl: reader.result })
-      img.src = dataURL = reader.result
-    }
-    img.onload = () => {
-      let canvas = document.createElement('canvas')
-      let ctx = canvas.getContext('2d')
-      let tempimg = new Image()
-      tempimg.src = img.src
-      let w = tempimg.width
-      let h = tempimg.height
-      if (w > config.data.max_side || h > config.data.max_side) {
-        if (w > h) {
-          if (w > config.data.max_side) {
-            h = (h * config.data.max_side) / w
-            w = config.data.max_side
-          }
-        } else {
-          if (h > config.data.max_side) {
-            w = (w * config.data.max_side) / h
-            h = config.data.max_side
+    let img = new Image()
+    getOrientation(f, or => {
+      let reader = new FileReader()
+      reader.readAsDataURL(f)
+      reader.onload = () => { img.src = reader.result }
+      img.onload = () => {
+        let canvas = document.createElement('canvas')
+        let ctx = canvas.getContext('2d')
+        let w = img.width
+        let h = img.height
+        if (w > config.data.max_side || h > config.data.max_side) {
+          if (w > h) {
+            if (w > config.data.max_side) {
+              h = (h * config.data.max_side) / w
+              w = config.data.max_side
+            }
+          } else {
+            if (h > config.data.max_side) {
+              w = (w * config.data.max_side) / h
+              h = config.data.max_side
+            }
           }
         }
-        canvas.height = h
-        canvas.width = w
+        if (or > 4 && or < 9) {
+          canvas.width = h
+          canvas.height = w
+        } else {
+          canvas.width = w
+          canvas.height = h
+        }
+        switch (or) {
+        case 2: ctx.transform(-1, 0, 0, 1, w, 0); break
+        case 3: ctx.transform(-1, 0, 0, -1, w, h); break
+        case 4: ctx.transform(1, 0, 0, -1, 0, h); break
+        case 5: ctx.transform(0, 1, 1, 0, 0, 0); break
+        case 6: ctx.transform(0, 1, -1, 0, h, 0); break
+        case 7: ctx.transform(0, -1, -1, 0, h, w); break
+        case 8: ctx.transform(0, -1, 1, 0, 0, w); break
+        default: break
+        }
         ctx.drawImage(img, 0, 0, w, h)
-        dataURL = canvas.toDataURL()
+        let dataURL = canvas.toDataURL()
+        this.setState({imagePreviewUrl: dataURL, file: dataURLtoFile(dataURL, 'media.png')})
       }
-      let res = dataURLtoFile(dataURL, 'media.png')
-      this.setState({file: res})
-    }
+    })
   }
   render () {
     let $imagePreview = null
