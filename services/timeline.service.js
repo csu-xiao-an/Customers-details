29aacbbdea4e13b349a809dff01e58378583100c
vipslord <my.yershov@gmail.com>
2017-12-03 22:19:50 +0200

    add requests

--- services/timeline.service.js
+++ services/timeline.service.js
@@ -1,7 +1,39 @@
 import mainRequestService from './request.service'
 
-export default () => {
-  const url = config.urls.main + '/clients/123123/appoinments'
+export const appointments = () => {
+  const url = config.urls.main + '/customers-details/clients/123/timeline/appointments?start=2017-01-01&end=2017-01-07'
+  const options = {
+    mode: 'cors',
+    method: 'GET'
+  }
+  return mainRequestService(url, options)
+}
+export const gallery = () => {
+  const url = config.urls.main + '/customers-details/clients/123/timeline/gallery?start=2017-01-01&end=2017-01-07'
+  const options = {
+    mode: 'cors',
+    method: 'GET'
+  }
+  return mainRequestService(url, options)
+}
+export const depts = () => {
+  const url = config.urls.main + '/customers-details/clients/123/timeline/depts?start=2017-01-01&end=2017-01-07'
+  const options = {
+    mode: 'cors',
+    method: 'GET'
+  }
+  return mainRequestService(url, options)
+}
+export const notes = () => {
+  const url = config.urls.main + '/customers-details/clients/123/timeline/notes?start=2017-01-01&end=2017-01-07'
+  const options = {
+    mode: 'cors',
+    method: 'GET'
+  }
+  return mainRequestService(url, options)
+}
+export const sms = () => {
+  const url = config.urls.main + '/customers-details/clients/123/timeline/sms?start=2017-01-01&end=2017-01-07'
   const options = {
     mode: 'cors',
     method: 'GET'
