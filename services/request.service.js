17d49340c0ff6d97de72ecd0289b1a7855b4356a
vipslord <my.yershov@gmail.com>
2018-04-02 19:09:48 +0300

    406820453753043, Imported Request service from Components

--- services/request.service.js
+++ services/request.service.js
@@ -1,3 +1,3 @@
-const mainRequestService = (u, o) => fetch(u, o).then(r =>
-  r.status !== 503 ? r : new Promise(resolve => setTimeout(() => resolve(mainRequestService(u, o)), r.headers.get('Retry-After'))))
-export default mainRequestService
+import { requestService } from 'project-components'
+
+export default requestService
