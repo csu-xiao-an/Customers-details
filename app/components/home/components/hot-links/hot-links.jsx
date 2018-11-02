e6feef5e7512cbe924fdeec5ebebacd8a64eaa14
Denis Tsekhanovskiy <denisamk44@gmail.com>
2018-11-02 14:35:59 +0200

    removed swiper from hot-links

--- app/components/home/components/hot-links/hot-links.jsx
+++ app/components/home/components/hot-links/hot-links.jsx
@@ -1,4 +1,3 @@
-import { Swiper } from 'project-components'
 import './hot-links.styl'
 const { Link } = ReactRouterDOM
 
@@ -30,7 +29,7 @@ export default class HotLinks extends React.Component {
     let scroll = e.scrollIntoView({ behavior: 'smooth', block: 'start' })
   }
   renderExternalLink = (url, name, img) => (
-    <div>
+    <div className='link-wrap'>
       <Link className={'link ' + (this.props.rights.hot_links.external ? 'square' : 'hidden')} to={config.baseUrl ? config.baseUrl.replace('{client_id}', config.data.id) + url : url}><img src={img} /></Link>
       <span>{name}</span>
     </div>
@@ -38,35 +37,33 @@ export default class HotLinks extends React.Component {
   render () {
     return (
       <div id='hot-links'>
-        <Swiper slidesPerView='auto'>
-          {config.data.hot_links.map(i => {
-            if (i.url[0] === '#') {
-              return (
-                <div>
-                  <div onClick={() => this.link(i)}
-                    className={'link ' + (this.props.rights.hot_links.internal ? 'circle1' : 'hidden')}>
-                    <img src={i.img} />
-                  </div>
-                  <span>{config.translations.hot_links[i.name]}</span>
+        {config.data.hot_links.map(i => {
+          if (i.url[0] === '#') {
+            return (
+              <div className='link-wrap'>
+                <div onClick={() => this.link(i)}
+                  className={'link ' + (this.props.rights.hot_links.internal ? 'circle1' : 'hidden')}>
+                  <img src={i.img} />
                 </div>
-              )
+                <span>{config.translations.hot_links[i.name]}</span>
+              </div>
+            )
+          } else {
+            if (i.url === config.urls.punch_cards) {
+              return this.state.isActivePunchCard
+                ? this.renderExternalLink(i.url, config.translations.hot_links[i.name], i.img)
+                : this.renderExternalLink(config.urls.punch_cards_adding, config.translations.punch_cards_adding, '')
             } else {
-              if (i.url === config.urls.punch_cards) {
-                return this.state.isActivePunchCard
-                  ? this.renderExternalLink(i.url, config.translations.hot_links[i.name], i.img)
-                  : this.renderExternalLink(config.urls.punch_cards_adding, config.translations.punch_cards_adding, '')
-              } else {
-                return this.renderExternalLink(i.url, config.translations.hot_links[i.name], i.img)
-              }
+              return this.renderExternalLink(i.url, config.translations.hot_links[i.name], i.img)
             }
-          })}
-          {/* <div>
-            <div className='link add-btn'>
-              <img className='add' src={config.urls.media + 'ic_add.png'} />
-            </div>
-            <span>{config.translations.add_hot_line}</span>
-          </div> */}
-        </Swiper>
+          }
+        })}
+        {/* <div>
+          <div className='link add-btn'>
+            <img className='add' src={config.urls.media + 'ic_add.png'} />
+          </div>
+          <span>{config.translations.add_hot_line}</span>
+        </div> */}
       </div>
     )
   }
