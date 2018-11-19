81a8f57c5d48d7c1dd2126ff37bda92b781db5d6
Denis Tsekhanovskiy <denisamk44@gmail.com>
2018-11-19 15:01:58 +0200

    readonly renamed checkok -> checkbox deleted handleAds

--- app/components/home/components/agreement/agreement.jsx
+++ app/components/home/components/agreement/agreement.jsx
@@ -6,15 +6,15 @@ export default class Agreement extends React.Component {
     isChecked: false
   }
 
-  handleAds = () => {
-    const body = `${config.translations.permit_ads}=${!config.data.permit_ads}`
-    clientReplaceService(body).then(r => {
-      if (r.status === 204) {
-        config.data.permit_ads = !config.data.permit_ads
-        this.forceUpdate()
-      }
-    })
-  }
+  // handleAds = () => {
+  //   const body = `${config.translations.permit_ads}=${!config.data.permit_ads}`
+  //   clientReplaceService(body).then(r => {
+  //     if (r.status === 204) {
+  //       config.data.permit_ads = !config.data.permit_ads
+  //       this.forceUpdate()
+  //     }
+  //   })
+  // }
 
   render () {
     return (
@@ -24,8 +24,8 @@ export default class Agreement extends React.Component {
             <span className='label'>{config.translations.agreement}:</span>
           </div>
           <div className={'check ' + (config.isRtL ? 'right' : 'left')} onClick={this.handleAds}>
-            <img src={config.urls.media + (config.data.permit_ads ? 'chekbox-unmarked.svg' : 'chekbox-marked.svg')} />
-            <img className={`checking ${config.data.permit_ads ? 'show' : ''}`} src={config.urls.media + 'checkok.svg'} />
+            <img src={config.urls.media + (config.data.permit_ads ? 'checkbox-unmarked.svg' : 'checkbox-marked.svg')} />
+            <img className={`checking ${config.data.permit_ads ? 'show' : ''}`} src={config.urls.media + 'checkbox.svg'} />
           </div>
         </div>
       </div>
