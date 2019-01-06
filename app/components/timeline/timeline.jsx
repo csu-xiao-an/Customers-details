74b54e797728b2ff8130bb254b0e5b2a87a7760a
vipslord <my.yershov@gmail.com>
2019-01-06 14:20:13 +0200

    added buttons in timeline and fixed recursive upload

--- app/components/timeline/timeline.jsx
+++ app/components/timeline/timeline.jsx
@@ -1,13 +1,29 @@
 import Appointment from './components/appointment/appointment.jsx'
 import AccessRights from '../access-rights/access-rights.jsx'
+import PunchCard from './components/punch-card/punch-card.jsx'
+import Gallery from './components/gallery/gallery.jsx'
+import Debt from './components/debt/debt.jsx'
+import Note from './components/note/note.jsx'
+// import { Swiper } from 'project-components'
+import Sms from './components/sms/sms.jsx'
 import Topnav from '../topnav/topnav.jsx'
 import {getTimeline} from 'project-services'
 import './timeline.styl'
 
-const urlParams = ['appointments', 'sms', 'notes']
+let urlParams = ['appointments', 'sms', 'notes']
 
 class Timeline extends React.Component {
   state = {
+    filter: {
+      appointments: true,
+      punch_cards: true,
+      gallery: true,
+      other: true,
+      debts: true,
+      sms: true,
+      notes: true
+
+    },
     flag: false,
     data: []
   }
@@ -15,7 +31,7 @@ class Timeline extends React.Component {
 
   componentDidMount = () => {
     if (config.isRtL) document.getElementsByTagName('body')[0].style.direction = 'rtl'
-    config.plugins_list.forEach(i => urlParams.push(i))
+    config.plugins_list.forEach(i => i !== 'colors_beautech' && urlParams.push(i))
     this.getData(true, true)
     this.init()
   }
@@ -34,7 +50,7 @@ class Timeline extends React.Component {
     const { offsetHeight } = this.refs.list
     const { clientHeight: ch } = document.body
     if (offsetHeight <= ch) { // 1.5 page size. Universal value
-      this.getData(false, Math.pow(config.interval_days, 2))
+      this.getData(false, config.interval_days * 2)
     }
   }
 
@@ -65,28 +81,30 @@ class Timeline extends React.Component {
     this.lastStartDate = start
     this.lastEndDate = end
     getTimeline(urlParams, { end, start }).then(res => {
-      let data = res.appointments
-      // let keys = Object.keys(res)
-      // let testData = keys.map(i => {
-      //   let a = [...res[i]]
-      //   return a
-      // })
-      // let data = testData.reduce((arr, item) => arr.concat(item.map(i => {
-      //   console.log('i', i)
-      //   if (!i.date) {
-      //     i.date = moment(i.start).format('YYYY-MM-DD')
-      //   }
-      // if (!this.state.filter.sms && i.field_name === 'sms') { i.isHide = true } else
-      // if (!this.state.filter.notes && i.field_name === 'notes') { i.isHide = true } else
-      // if (!this.state.filter.appointments && i.field_name === 'appointments') { i.isHide = true }
-      //   return i
-      // })), [])
+      // let data = res.appointments
+      let keys = Object.keys(res)
+      let overallData = keys.map(i => {
+        let a = [...res[i]]
+        return a
+      })
+      let data = overallData.reduce((arr, item) => arr.concat(item.map(i => {
+        if (!i.date) {
+          i.date = moment(i.start).format('YYYY-MM-DD')
+        }
+        if (!this.state.filter.sms && i.field_name === 'sms') { i.isHide = true } else
+        if (!this.state.filter.notes && i.field_name === 'notes') { i.isHide = true } else
+        if (!this.state.filter.appointments && i.field_name === 'appointments') { i.isHide = true } else
+        if (!this.state.filter.punch_cards && i.field_name === 'punch_cards') { i.isHide = true } else
+        if (!this.state.filter.gallery && i.field_name === 'gallery') { i.isHide = true } else
+        if (!this.state.filter.debts && i.field_name === 'debts') i.isHide = true
+        return i
+      })), [])
       if (data.length) {
-        data.sort((a, b) => moment(b.start) - moment(a.start))
+        data.sort((a, b) => moment(b.date) - moment(a.date))
         data[0].separator = true
         const format = date => moment(date).format('YYYY-MM-DD')
         data.reduce((pI, cI) => {
-          if (format(pI.start) !== format(cI.start)) cI.separator = true
+          if (format(pI.date) !== format(cI.date)) cI.separator = true
           return cI
         })
       }
@@ -96,25 +114,84 @@ class Timeline extends React.Component {
       )
     })
   }
+  filter = p => {
+    const f = b => {
+      this.setState({data: this.state.data.map(i => {
+        if (p === 'appointments' && i.field_name === 'appointments') { i.isHide = b } else
+        if (p === 'punch_cards' && i.field_name === 'punch_cards') { i.isHide = b } else
+        if (p === 'gallery' && i.field_name === 'gallery') { i.isHide = b } else
+        if (p === 'debts' && i.field_name === 'debts') i.isHide = b
+        if (p === 'sms' && i.field_name === 'sms') { i.isHide = b } else
+        if (p === 'notes' && i.field_name === 'notes') { i.isHide = b }
+        return i
+      }),
+      filter: { ...this.state.filter, [p]: !b }})
+    }
+    this.state.filter[p] ? f(true) : f(false)
+  }
   render () {
     const fields = {
-      appointments: i => <Appointment i={i} {...this.props} />
+      appointments: i => <Appointment i={i} {...this.props} />,
+      punch_cards: i => <PunchCard i={i} {...this.props} />,
+      gallery: i => <Gallery i={i} {...this.props} />,
+      debts: i => <Debt i={i} {...this.props} />,
+      notes: i => <Note i={i} {...this.props} />,
+      sms: i => <Sms i={i} {...this.props} />
     }
     return (
       <div id='timeline'>
         <Topnav {...this.props} timeline />
         <div className='list' ref='list'>
-          {this.state.data.length > 0 && this.state.data.map(i => <div>
+          {this.state.data.length > 0 && this.state.data.map(i => !i.isHide && i.field_name === 'appointments' && <div>
             {i.separator && <div className='separator-wrap'><div className='separator'>
-              <span className='date_weekday'>{`${config.translations.dates.weekdays[moment(i.start).get('day')]},`}</span>
-              <span className='date_month'>{config.translations.dates.months[moment(i.start).get('month')]}</span>
-              <span className='date_day'>{moment(i.start).format('DD')}</span>
+              <span className='date_weekday'>{`${config.translations.dates.weekdays[moment(i.date).get('day')]},`}</span>
+              <span className='date_month'>{config.translations.dates.months[moment(i.date).get('month')]}</span>
+              <span className='date_day'>{moment(i.date).format('DD')}</span>
             </div>
             </div>}
             { fields[i.field_name] && fields[i.field_name](i) }
           </div>)}
           <div className={this.state.flag ? 'preloader' : 'hidden'}>
             <div className='loader' />
+          </div>
+        </div>
+        <div className='buttons-wrap'>
+          <div className='buttons-container'>
+            <div className='single-button' onClick={() => this.filter('appointments')}>
+              <img className={`mark ${this.state.filter.appointments ? '' : 'nVisible'}`} src={config.urls.media + 'ic-mark.svg'} />
+              <img className='main-img' src={`${config.urls.media}ic-q.svg`} />
+              <p>{config.translations.queues}</p>
+            </div>
+            {config.plugins_list.some(i => i === 'gallery') && this.props.rights.timeline.isVisibleGalleryButton &&
+            <div className='single-button' onClick={() => this.filter('gallery')}>
+              <img className={`mark ${this.state.filter.gallery ? '' : 'nVisible'}`} src={config.urls.media + 'ic-mark.svg'} />
+              <img className='main-img' src={`${config.urls.media}ic-g.svg`} />
+              <p>{config.translations.gallery}</p>
+            </div>}
+            {config.plugins_list.some(i => i === 'debts') && <div className='single-button' onClick={() => this.filter('debts')}>
+              <img className={`mark ${this.state.filter.debts ? '' : 'nVisible'}`} src={config.urls.media + 'ic-mark.svg'} />
+              <img className='main-img' src={`${config.urls.media}ic-d.svg`} />
+              <p>{config.translations.debts_t}</p>
+            </div>}
+            {/* <div onClick={() => this.filter('other')}>
+              <img className={this.state.filter.other ? '' : 'nVisible'} src={config.urls.media + 'ic-mark.svg'} />
+              <span>{config.translations.other_t}</span>
+            </div> */}
+            {config.plugins_list.some(i => i === 'punch_cards') && <div className='single-button' onClick={() => this.filter('punch_cards')}>
+              <img className={`mark ${this.state.filter.punch_cards ? '' : 'nVisible'}`} src={config.urls.media + 'ic-mark.svg'} />
+              <img className='main-img' src={`${config.urls.media}ic-sub.svg`} />
+              <p>{config.translations.punch_cards}</p>
+            </div>}
+            <div className='single-button' onClick={() => this.filter('notes')}>
+              <img className={`mark ${this.state.filter.notes ? '' : 'nVisible'}`} src={config.urls.media + 'ic-mark.svg'} />
+              <img className='main-img' src={`${config.urls.media}ic-n.svg`} />
+              <p>{config.translations.note_t}</p>
+            </div>
+            <div className='single-button' onClick={() => this.filter('sms')}>
+              <img className={`mark ${this.state.filter.sms ? '' : 'nVisible'}`} src={config.urls.media + 'ic-mark.svg'} />
+              <img className='main-img' src={`${config.urls.media}ic-s.svg`} />
+              <p>{config.translations.sms_t}</p>
+            </div>
           </div>
         </div>
       </div>
