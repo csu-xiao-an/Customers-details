2ff098bde230ff008a3999dd74413da42f0fa217
DenisKenzo <denisamk44@gmail.com>
2019-01-23 14:52:01 +0200

    edit personal info not allowed on readonly now

--- app/components/home/components/profile/profile.jsx
+++ app/components/home/components/profile/profile.jsx
@@ -105,7 +105,7 @@ render () {
     <div id='profile'>
       <div id='profile-header'>
         <h2 className='block-title'>{config.translations.profile}</h2>
-        {this.state.editProfile 
+        {/* {this.state.editProfile 
           ? <div className='back-save-btn'>
             <div className='back-wrap'>
               <div className='img-back'><img src={config.urls.media + 'arrow-left.svg'} /></div>
@@ -119,7 +119,7 @@ render () {
           : <div className='edit-profile' onClick={() => this.setState({editProfile: true})}>
             <div className='edit-wrap'><img className='img-edit-profile' src={config.urls.media + 'edit-note.svg'} /></div>
             <div>{config.translations.editProfile}</div>
-          </div>}
+          </div>} */}
       </div>
       {(this.props.isVisibleFields || config.data.name) && <div id='name'>
         {!this.state.editProfile ? <div className='fullname'>
