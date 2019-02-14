0d8e9401404285d6e38813a30e3a41250f65a705
vipslord <my.yershov@gmail.com>
2019-02-14 12:06:35 +0200

    delete address_srt from config.urls

--- config.js
+++ config.js
@@ -325,7 +325,6 @@ var config = {
     timeline: '/timeline',
     groups: '/groups/',
     login: '/en/login',
-    address_srt: 'address',
     status: 'status',
     source: 'source',
     gender: 'gender',
