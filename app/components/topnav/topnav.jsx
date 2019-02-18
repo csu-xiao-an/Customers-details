5be80e8be92c8112b3d2e9d137649da7103b1642
DenisKenzo <denisamk44@gmail.com>
2019-02-18 12:06:41 +0200

    fix menu direction

--- app/components/topnav/topnav.jsx
+++ app/components/topnav/topnav.jsx
@@ -48,8 +48,11 @@ export default class Topnav extends React.Component {
     return (
       <div id='topnav'>
         <div className='header' >
-          <div className={'arrow-wrap ' + (config.isRTL ? 'rtl' : 'ltr')} onClick={this.props.rights.topnav.back ? () => window.history.go(-1) : () => {}}>
-            <img className='arrow-back' src={config.urls.media + 'arrow-back.svg'} style={config.isRTL ? {transform: 'scale(-1, 1)'} : {}} /></div>
+          <div className={'edit-wrap ' + (config.isRTL ? 'rtl' : 'ltr')} style={!config.isRTL ? {transform: 'scale(-1, 1)'} : {}}>
+            {this.props.home && <img className='edit' onClick={this.menuOnOff} src={config.urls.media + 'ic_menu.svg'} />}
+            {this.props.punch && <img className='add' src={config.urls.media + 'add_bt.svg'}
+              onClick={() => this.props.history.push(config.urls.punch_cards_adding)} />}
+          </div>
           {(this.props.home || this.props.timeline) && <div className='client-name'>
             <div className='icon-online' />
             <h1>{config.data.name}</h1>
@@ -59,11 +62,8 @@ export default class Topnav extends React.Component {
             <h1>{config.translations.punch_topnav.replace('{client_name}', config.data.name)}</h1></div>}
           {this.props.color && <div className='client-name'>
             <h1>{config.translations.color_card_topnav.replace('{client_name}', config.data.name)}</h1></div>}
-          <div className={'edit-wrap ' + (config.isRTL ? 'ltr' : 'rtl')} style={config.isRTL ? {transform: 'scale(-1, 1)'} : {}}>
-            {this.props.home && <img className='edit' onClick={this.menuOnOff} src={config.urls.media + 'ic_menu.svg'} />}
-            {this.props.punch && <img className='add' src={config.urls.media + 'add_bt.svg'}
-              onClick={() => this.props.history.push(config.urls.punch_cards_adding)} />}
-          </div>
+          <div className={'arrow-wrap ' + (config.isRTL ? 'ltr' : 'rtl')} onClick={this.props.rights.topnav.back ? () => window.history.go(-1) : () => {}}>
+            <img className='arrow-back' src={config.urls.media + 'arrow-back.svg'} style={!config.isRTL ? {transform: 'scale(-1, 1)'} : {}} /></div>
         </div>
         {this.state.isActive && <Menu closeMenu={this.closeMenu} />}
       </div>
