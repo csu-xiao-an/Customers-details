873fbb057ff12fc701f8b716a86b8b76e11597b6
DenisKenzo <denisamk44@gmail.com>
2019-04-05 17:39:19 +0300

    if uses.length = service_count punch card is used now new text in button if punch card is used minor fixes style

--- app/components/punch-cards/components/single-punch-page/single-punch-page.jsx
+++ app/components/punch-cards/components/single-punch-page/single-punch-page.jsx
@@ -17,7 +17,7 @@ export default class SinglePunchPage extends React.Component {
       this.setState({punchsList}, () => {
         this.setState({
           singlePunch: this.state.punchsList.find(i => i.id === +this.props.match.params.punch_card_id) || {},
-          uses: this.state.punchsList.uses && (this.state.punchsList.find(i => i.id === +this.props.match.params.punch_card_id).uses || [])
+          uses: this.state.punchsList.find(i => i.id === +this.props.match.params.punch_card_id).uses || []
         })
       })
     })
@@ -39,9 +39,8 @@ export default class SinglePunchPage extends React.Component {
   update = () => this.forceUpdate()
   use = () => {
     const d = moment().format('YYYY-MM-DD hh:mm:ss')
-    punchPostServiceUse(this.state.singlePunch.id, `date=${d}`).then(r => r.json().then(r => {
-      this.state.punchsList.uses && this.setState(state => ({ uses: [{ id: r, date: d }, ...state.uses] }))
-    }))
+    punchPostServiceUse(this.state.singlePunch.id, `date=${d}`).then(r => r.json().then(r =>
+      this.setState(state => ({ uses: [{ id: r, date: d }, ...state.uses] }))))
   }
   render () {
     const { singlePunch = {}, uses = [] } = this.state
@@ -58,13 +57,20 @@ export default class SinglePunchPage extends React.Component {
                 <p className='punch-name'>{singlePunch.service_name}</p>
               </div>
               <div className='punch'>
-                <p className='count'><span>{config.translations.used}</span><span className='uses'>{uses ? uses.length : '0'}</span><span className='of'>{config.translations.of}</span><span className={'total ' + (this.daysLeft() > 0 && 'unused')} >{singlePunch.service_count}</span></p>
+                <p className='count'><span>{config.translations.used}</span>
+                  <span className='uses'>{uses ? uses.length : '0'}</span>
+                  <span className='of'>{config.translations.of}</span>
+                  <span className={'total ' + (this.daysLeft() > 0 && 'unused')} >{singlePunch.service_count}</span>
+                </p>
               </div>
               <div className={'sum ' + (config.isRTL && 'sum-rtl')}><p>{singlePunch.sum}</p><p className='currency'>{config.data.currency}</p></div>
             </div>
             {this.daysLeft() > 0
               ? <button className='expiry-btn'>{config.translations.expiry_dates}</button>
-              : <button className='use-btn' onClick={(this.state.uses && this.state.uses.length === singlePunch.service_count) || this.daysLeft() > 0 ? () => {} : this.use} >{config.translations.use}<img src={config.urls.media + 'check-circle.svg'} /></button>}
+              : (this.state.uses && this.state.uses.length !== singlePunch.service_count ? <button className='use-btn'
+                onClick={(this.state.uses && this.state.uses.length === singlePunch.service_count) || this.daysLeft() > 0 ? () => {} : this.use} >{config.translations.use}
+                <img src={config.urls.media + 'check-circle.svg'} /></button>
+                : <button className='expiry-btn'>No more uses in this punch card</button>)}
             {singlePunch.expiration && <div className='expiry-date'>
               <div className='img-wrap'><img src={config.urls.media + 'calendar.svg'} /></div>
               {this.daysLeft() > 0 ? <div className='expiry-text'>
