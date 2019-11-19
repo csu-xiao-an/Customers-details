08c443a30e91ec58944285f1338959351246b98a
tkachuks <serhiytkachuk88@gmail.com>
2019-11-19 14:55:22 +0200

    returned listener on scroll

--- app/components/topnav/topnav.jsx
+++ app/components/topnav/topnav.jsx
@@ -39,14 +39,18 @@ export default class Topnav extends React.Component {
       const arr = document.querySelectorAll('.list-item')
       const menuModal = document.querySelector('#menu_modal')
       const lastItem = arr[arr.length - 1]
-      const coords = lastItem.getBoundingClientRect()
-      const bottomCoord = coords.bottom - coords.height / 3
-      console.log('coords', coords)
-      console.log('bottomCoord', Math.floor(bottomCoord))
-      console.log('clientHeight', menuModal.clientHeight)
-      if (Math.floor(bottomCoord) >= menuModal.clientHeight) {
-        lastItem.style.marginBottom = '4rem'
-      }
+      menuModal.addEventListener('scroll', () => {
+        if (menuModal.scrollTop == menuModal.scrollHeight - menuModal.clientHeight) {
+          const coords = lastItem.getBoundingClientRect()
+          const bottomCoord = coords.bottom - coords.height / 3
+          console.log('coords', coords)
+          console.log('bottomCoord', Math.floor(bottomCoord))
+          console.log('clientHeight', menuModal.clientHeight)
+          if (Math.floor(bottomCoord) >= menuModal.clientHeight) {
+            lastItem.style.marginBottom = '4rem'
+          }
+        }
+      })
     })
     document.querySelector('body').classList.toggle('no-scroll')
   }
@@ -65,7 +69,7 @@ export default class Topnav extends React.Component {
             {this.props.home && <img className='edit' onClick={this.menuOnOff} src={config.urls.media + 'ic_menu.svg'} />}
           </div>
           {(this.props.home || this.props.timeline) && <div className='client-name'>
-            <h1>{config.data.name}</h1>
+            <h1>Name</h1>
             {birthdate && <span>{config.translations.birthdate_info.years_old.replace('{count}', birthdate)}</span>}
           </div>}
           <div className={'arrow-wrap ' + (config.isRTL ? 'ltr' : 'rtl')} onClick={this.props.rights.topnav.back ? () => window.history.go(-1) : () => {}}>
